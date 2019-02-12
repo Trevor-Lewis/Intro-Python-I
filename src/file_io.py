73264d9038a6dec4f39184c192c7fac1e2352e4f
Trevor Lewis <tenzingtitan@gmail.com>
2019-02-12 15:52:40 -0700

    file_IO finished

--- src/file_io.py
+++ src/file_io.py
@@ -10,9 +10,30 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+with open('src/foo.txt', 'r') as f1:
+    print(f1.read())
+
+# f1 = open('src/foo.txt', 'r')
+# print(f1)
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make 
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+contents = '''
+This is some arbitrary content
+
+It's cool I guess
+
+Not as cool as Python
+'''
+
+with open('src/bar.txt', 'w') as f2:
+    f2.write(contents)
+
+with open('bar.txt') as f3:
+    if f3.read() == contents:
+        print("It worked!")
